8060fdfb4a4e993975ae5ea79d3b6a55631da00c
stassajin <sajin.stanislav@gmail.com>
2017-10-10 07:37:58 -0700

    added lcm solution

--- algo_toolbox/fibonacci_huge.py
+++ algo_toolbox/fibonacci_huge.py
@@ -5,12 +5,11 @@ def get_fibonacci_huge_naive(n, m):
     if n <= 1:
         return n
 
-    previous = 0
+    previous = 1
     current  = 1
-
     for _ in range(n - 1):
-        previous, current = current, previous + current
-
+        while previous != 0 and current !=1:
+            previous, current = current, previous + current % m
     return current % m
 
 if __name__ == '__main__':
--- algo_toolbox/lcm.py
+++ algo_toolbox/lcm.py
@@ -1,10 +1,17 @@
 # Uses python3
 import sys
 
+def EuclidGCD(a, b):
+    # this is version 3 implementation using Euclidian method
+    if b == 0:
+        return a
+    a = a % b
+    return(EuclidGCD(b, a))
+
 def lcm(a, b):
-    smallest = a if a < b else b
-    largest = a if a > b else b
-    
+    # and the lcf = a*b/gcf
+    gcf = EuclidGCD(a, b)
+    return a*b//gcf
 
 if __name__ == '__main__':
     input = sys.stdin.read()
