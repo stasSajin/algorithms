57a05ea13a93763b868b94349410c6ca585e8f8c
Stas Sajin <sajin.stanislav@gmail.com>
2017-10-10 15:39:25 -0700

    added some info on partial sum

--- algo_toolbox/fibonacci_partial_sum.py
+++ algo_toolbox/fibonacci_partial_sum.py
@@ -1,22 +1,26 @@
 # Uses python3
 import sys
 
-def fibonacci_partial_sum_naive(from_, to):
-    sum = 0
+def fibonacci_partial_sum(a, b):
+    previous = 1
+    current  = 1
+    cycle_len = 1 # starting at 1 since out `previous` starts at 1 rather than 0
+    # set up the initial sequence of numbers
+    f_modulo = [0, 1, 1]
+    for _ in range(2, a):
+        cycle_len += 1
+        previous, current = current, (previous + current) % 10
+        f_modulo.append(current)
+        if previous == 0 and current == 1:
+            f_modulo.pop()
+            f_modulo.pop()
+            rem = n % cycle_len
+            return b + rem/cycle_len - 1
 
-    current = 0
-    next  = 1
-
-    for i in range(to + 1):
-        if i >= from_:
-            sum += current
-
-        current, next = next, (current + next) % 10
-
-    return sum % 10
-
+    # if the n of fibonacy_modulo is smaller than the cycle, return f[n]
+    return sum(f_modulo)%10
 
 if __name__ == '__main__':
     input = sys.stdin.read();
     from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    print(fibonacci_partial_sum_naive(a, b))
