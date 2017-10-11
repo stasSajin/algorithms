3fe2b5f8b2c161d84bfd777b44ddbebc7cb05e35
stassajin <sajin.stanislav@gmail.com>
2017-10-10 18:00:12 -0700

    added solutions to all the fib problems

--- algo_toolbox/fibonacci_huge.py
+++ algo_toolbox/fibonacci_huge.py
@@ -1,27 +1,19 @@
 # Uses python3
 import sys
 
-def get_fibonacci_huge(n, m):
+def get_fibonacci_huge_naive(n, m):
     if n <= 1:
         return n
 
-
-    previous = 1
+    previous = 0
     current  = 1
-    cycle_len = 1 # starting at 1 since out `previous` starts at 1 rather than 0
-    # set up the initial sequence of numbers
-    f_modulo = [0, 1, 1]
-    for _ in range(2, n):
-        cycle_len += 1
-        previous, current = current, (previous + current) % m
-        f_modulo.append(current)
-        if previous == 0 and current == 1:
-            new_n = n % cycle_len
-            return f_modulo[new_n]
-    # if the n of fibonacy_modulo is smaller than the cycle, return f[n]
-    return f_modulo[n]
+
+    for _ in range(n - 1):
+        previous, current = current, previous + current % m
+
+    return current % m
 
 if __name__ == '__main__':
     input = sys.stdin.read();
     n, m = map(int, input.split())
-    print(get_fibonacci_huge(n, m))
+    print(get_fibonacci_huge_naive(n, m))
--- algo_toolbox/fibonacci_partial_sum.py
+++ algo_toolbox/fibonacci_partial_sum.py
@@ -7,20 +7,26 @@ def fibonacci_partial_sum(a, b):
     cycle_len = 1 # starting at 1 since out `previous` starts at 1 rather than 0
     # set up the initial sequence of numbers
     f_modulo = [0, 1, 1]
-    for _ in range(2, a):
+    for _ in range(2, b):
         cycle_len += 1
         previous, current = current, (previous + current) % 10
         f_modulo.append(current)
         if previous == 0 and current == 1:
-            f_modulo.pop()
-            f_modulo.pop()
-            rem = n % cycle_len
-            return b + rem/cycle_len - 1
+            f_modulo = f_modulo[:-2]
+            rem_a = a % cycle_len
+            rem_b = b % cycle_len
+
+            # generate sum of digits from f to end of nearest cycle
+            sum_a = sum(f_modulo[rem_a:])
+            # generate sum of digits from start of nearest cycles to f2
+            sum_b = sum(f_modulo[:rem_b+1])
+            # generate sum digits for complete cycles between f1 and f2
+            return (sum_a + sum_b) % 10
 
     # if the n of fibonacy_modulo is smaller than the cycle, return f[n]
-    return sum(f_modulo)%10
+    return sum(f_modulo[a:b+1]) % 10
 
 if __name__ == '__main__':
     input = sys.stdin.read();
-    from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(a, b))
+    a, b = map(int, input.split())
+    print(fibonacci_partial_sum(a, b))
