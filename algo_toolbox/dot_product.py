fa9f12d28a6a10d2e8887c609616585e4356047f
stassajin <sajin.stanislav@gmail.com>
2017-10-15 16:45:15 -0700

    implemented the dot product

--- algo_toolbox/dot_product.py
+++ algo_toolbox/dot_product.py
@@ -5,8 +5,13 @@ import sys
 def max_dot_product(a, b):
     #write your code here
     res = 0
-    for i in range(len(a)):
-        res += a[i] * b[i]
+    # sort a and b
+    a = sorted(a, reverse=True)
+    b = sorted(b, reverse=True)
+    i = 0
+    while i < len(a):
+        res += a[i]*b[i]
+        i+=1
     return res
 
 if __name__ == '__main__':
@@ -16,4 +21,3 @@ if __name__ == '__main__':
     a = data[1:(n + 1)]
     b = data[(n + 1):]
     print(max_dot_product(a, b))
-    
